552588d2d343056edcecb1738f1146a53b064987
Jideotetic <jideotetic@gmail.com>
2025-03-04 15:41:44 +0100

    Text color fixes

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
         <div className="border p-4 rounded border-neutral-900 flex-1">
           <div className="mb-2 flex items-center justify-between">
             <span>From</span>
-            <span className="text-[#6D7A86]">16.979584 USDT</span>
+            <span className="text-neutral-300">16.979584 USDT</span>
           </div>
           <div className="bg-[#04131F] rounded p-2 flex justify-between items-center">
             <div className="flex items-center gap-2">
@@ -61,7 +61,7 @@ function App() {
         <div className="border p-4 rounded border-neutral-900 flex-1">
           <div className="mb-2 flex items-center justify-between">
             <span>To</span>
-            <span className="text-[#6D7A86]">16.979584 BCH</span>
+            <span className="text-neutral-300">16.979584 BCH</span>
           </div>
           <div className="bg-[#04131F] rounded p-2 flex justify-between items-center">
             <div className="flex items-center gap-2">
@@ -75,13 +75,13 @@ function App() {
 
       <div className="border rounded mt-6 p-4 border-neutral-900 flex flex-col sm:flex-row justify-between sm:items-center">
         <div className="flex gap-2 items-center">
-          <span className="text-[#6D7A86]">Price:</span>
+          <span className="text-neutral-300">Price:</span>
           <span className="shrink-0">0.0018 BCH</span>
           <span className="text-[#33ED8D]">Market</span>
         </div>
         <hr className="sm:hidden border border-neutral-900 my-4" />
         <div className="flex gap-2 items-center">
-          <span className="text-[#6D7A86] shrink-0">Order ID:</span>
+          <span className="text-neutral-300 shrink-0">Order ID:</span>
           <span>0xb10455fed0a9f4c782af</span>
         </div>
       </div>
@@ -91,61 +91,61 @@ function App() {
           <div className="border-b p-4 border-neutral-900 flex items-center gap-2">
             <img src={viteLogo} alt="" className="w-10 h-10" />
             <div className="flex flex-col">
-              <span className="text-[#6D7A86]">From:</span>
+              <span className="text-neutral-300">From:</span>
               <span>USDT</span>
             </div>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Actual order asset</span>
+            <span className="text-neutral-300">Actual order asset</span>
             <div className="flex items-center gap-1">
               <img src={viteLogo} alt="" className="w-5 h-5" />
               <span>USDT Coin</span>
             </div>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Amount</span>
+            <span className="text-neutral-300">Amount</span>
             <span>4,003.1 USDT</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Decimals</span>
+            <span className="text-neutral-300">Decimals</span>
             <span>18</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Maker</span>
+            <span className="text-neutral-300">Maker</span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Solver</span>
+            <span className="text-neutral-300">Solver</span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Creation tx</span>
+            <span className="text-neutral-300">Creation tx</span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Creation time</span>
+            <span className="text-neutral-300">Creation time</span>
             <span>04 Mar, 2025 • 12:35:19 PM</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">
+            <span className="text-neutral-300">
               Order authority in source chain
             </span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Order creator nonce</span>
+            <span className="text-neutral-300">Order creator nonce</span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Unlock Claim Tx</span>
+            <span className="text-neutral-300">Unlock Claim Tx</span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Solver unlock beneficiary</span>
+            <span className="text-neutral-300">Solver unlock beneficiary</span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Unlock Claim Time</span>
+            <span className="text-neutral-300">Unlock Claim Time</span>
             <span>04 Mar, 2025 • 2:17:46 PM</span>
           </div>
         </div>
@@ -153,63 +153,63 @@ function App() {
           <div className="border-b p-4 border-neutral-900 flex items-center gap-2">
             <img src={reactLogo} alt="" className="w-10 h-10" />
             <div className="flex flex-col">
-              <span className="text-[#6D7A86]">To:</span>
+              <span className="text-neutral-300">To:</span>
               <span>BCH</span>
             </div>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Asset</span>
+            <span className="text-neutral-300">Asset</span>
             <div className="flex items-center gap-1">
               <img src={reactLogo} alt="" className="w-5 h-5" />
               <span>BCH Coin</span>
             </div>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Amount</span>
+            <span className="text-neutral-300">Amount</span>
             <span>4,003.1 USDT</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Decimals</span>
+            <span className="text-neutral-300">Decimals</span>
             <span>18</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Solver</span>
+            <span className="text-neutral-300">Solver</span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Fulfill tx</span>
+            <span className="text-neutral-300">Fulfill tx</span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Fulfillment time</span>
+            <span className="text-neutral-300">Fulfillment time</span>
             <span>04 Mar, 2025 • 12:35:23 PM</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">
+            <span className="text-neutral-300">
               Order authority in source chain
             </span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Receiver address</span>
+            <span className="text-neutral-300">Receiver address</span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Unlock message</span>
+            <span className="text-neutral-300">Unlock message</span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Unlock time</span>
+            <span className="text-neutral-300">Unlock time</span>
             <span>04 Mar, 2025 • 12:35:23 PM</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">
+            <span className="text-neutral-300">
               Unlock authority in destination chain
             </span>
             <span>0x4264d7fcdcad03b578</span>
           </div>
           <div className="border-b p-4 border-neutral-900 flex flex-col gap-1 sm:flex-row justify-between sm:items-center">
-            <span className="text-[#6D7A86]">Status for Solver</span>
+            <span className="text-neutral-300">Status for Solver</span>
             <span>Order fulfilled and unlocked</span>
           </div>
         </div>
